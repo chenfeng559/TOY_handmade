7c9cb4659cccf1ac071698366d7effb6e660ab27
chengfeng <1258839505@qq.com>
2024-08-07 00:34:06 +0800

    在aduio-server中新增了audioRecognition函数

--- audio_server/tts_client.py
+++ audio_server/tts_client.py
@@ -7,7 +7,7 @@ import edge_tts
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode='threading',logging=True,engineio_logger = True,cors_allowed_origins="*")
+socketio = SocketIO(app, async_mode='threading',logging=True,engineio_logger = True,cors_allowed_origins='*')
 
 @socketio.on('connect')
 def connect():
@@ -28,18 +28,20 @@ def handle_text(text):
 async def handle_edge_tts(text):
     voice = "zh-CN-XiaoyiNeural"
     comunicate = edge_tts.Communicate(text,voice)
+    count = 0
     async for chunk in comunicate.stream():
-        print('chunk type =====>', chunk['type'])
-        if chunk['type'] == "audio" and chunk['data'] is not None:
+        if chunk['type'] == "audio":
             # print(chunk["data"])c
             try:
-                res = socketio.emit("audio", chunk["data"])
-                print('socket send=====>', res)
+                socketio.emit("count", count)
+                socketio.emit("audio", chunk["data"])
+                # count = count + 1
+                
             except Exception as e:
                 print(str(e))
                 print(socketio is None, chunk["data"])
         elif chunk['type'] == "WordBoundary":
-            print("end")
+            print(chunk)
             # break
 
 
